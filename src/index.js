ea1bbd142b01cd002a57f094115cee43c3b71700
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-09-18 21:05:37 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ app.get("/", (request, response) => {
 
 app.listen(3333, () => {
   console.log("===================================");
-  console.log("== SERVER RUNNING LIKE A ROCKET! ==");
+  console.log("== SER   dsdsVER RUNNING LIKE A ROCKET! ==");
   console.log("===================================");
 });
