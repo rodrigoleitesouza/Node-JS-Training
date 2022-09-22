368a3dbef736104eda3c48a991c0e9756bfe16e3
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-09-22 18:40:43 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
+app.get('/projects', (request, response) => {
+  return response.json([
+    'Project 1',
+    'Project 2',
+  ]);
+});
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Project 1',
+    'Project 2',
+    'Project 3',
+  ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'Project 4',
+    'Project 2',
+    'Project 3',
+  ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'Project 2',
+    'Project 3',
+  ]);
 });
 
 app.listen(3333, () => {
-  console.log("===================================");
-  console.log("== SERVER RUNNING LIKE A ROCKET! ==");
-  console.log("===================================");
+  console.log('===================================');
+  console.log('== SERVER RUNNING LIKE A ROCKET! ==');
+  console.log('===================================');
 });
