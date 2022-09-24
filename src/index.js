b6bc8f4242a4956739370f6136356e807faf567d
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-09-23 22:58:53 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -50,6 +50,6 @@ app.delete('/projects/:id', (request, response) => {
 
 app.listen(3333, () => {
   console.log('===================================');
-  console.log('== S ERVER RUNNING LIKE A ROCKET! ==');
+  console.log('== SERVER RUNNING LIKE A ROCKET! ==');
   console.log('===================================');
 });
